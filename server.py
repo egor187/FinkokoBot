4548d7c5d7021770d222f78d9210a5d2b5d37cce
egor187 <py.egor.py@gmail.com>
2021-07-26 15:37:51 +0300

    add endpoint for retrieving all categories from finbot

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import dotenv
 
 from aiogram import Bot, Dispatcher, executor, types
 
+import db
+
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 
@@ -18,7 +20,20 @@ dispatcher = Dispatcher(bot)
 
 @dispatcher.message_handler(commands=["start"])
 async def welcome(message: types.Message):
+    """Send welcome message"""
     await message.answer("Hi.\nI'm your fin bot!\nLet's start our budget")
+
+
+@dispatcher.message_handler(commands=["all_categories"])
+async def view_all_categories(message: types.Message):
+    """Retrieve all categories"""
+    answer = db.get_all_categories()
+    await message.answer(answer)
+
+
+@dispatcher.message_handler()
+async def view_all_payments(message: types.Message):
+    pass
 
 
 if __name__ == "__main__":
