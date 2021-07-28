0306a79ca53f113b4673bdac3cd172a0187f496e
egor187 <py.egor.py@gmail.com>
2021-07-29 00:06:57 +0300

    create handler for "add payments" process

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import dotenv
 from aiogram import Bot, Dispatcher, executor, types
 
 import db
+import exceptions
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
@@ -26,15 +27,31 @@ async def welcome(message: types.Message):
 
 @dispatcher.message_handler(commands=["all_categories"])
 async def view_all_categories(message: types.Message):
-    """Retrieve all categories"""
+    """Send message with all categories to user"""
     answer = db.get_all_categories()
     await message.answer(answer)
 
 
 @dispatcher.message_handler(commands=["payments"])
 async def view_month_payments(message: types.Message):
+    """Send message with all payments in last month"""
     result = db.get_payments_summary_for_categories_per_month()
     await message.answer(result)
+
+
+@dispatcher.message_handler()
+async def add_payment(message: types.Message):
+    """Entrypoint to 'add_payment' process"""
+    try:
+        db.add_payment_process(message)
+    except (exceptions.IncorrectAmountFormatMessage, exceptions.IncorrectMessageException) as e:
+        logging.info(e)
+        await message.answer("Incorrect message. Need in fmt '{amount} {category}'")
+    else:
+        await message.answer("Payment added")
+
+
+# TODO README.md
 
 
 if __name__ == "__main__":
