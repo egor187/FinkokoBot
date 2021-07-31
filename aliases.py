0b23f2127882a37acf02792f55d613410decebf5
egor187 <py.egor.py@gmail.com>
2021-07-31 23:56:32 +0300

    add aliases for categories

---
+++ aliases.py
@@ -0,0 +1,45 @@
+aliases = {
+    "продукты": "products",
+
+    "коммуналка": "communal",
+    "КП": "communal",
+
+    "кино": "entertainment",
+    "парк": "entertainment",
+    "театр": "entertainment",
+    "цирк": "entertainment",
+    "самокат": "entertainment",
+    "велосипед": "entertainment",
+    "скутер": "entertainment",
+    "опера": "entertainment",
+    "балет": "entertainment",
+    "планетарий": "entertainment",
+
+
+    "такси": "transport",
+    "taxi": "transport",
+    "самолет": "transport",
+    "поезд": "transport",
+    "метро": "transport",
+    "транспорт": "transport",
+
+    "бензин": "gasoline",
+    "заправка": "gasoline",
+
+    "СТО": "car",
+    "ТО": "car",
+
+    "mcdak": "cafe",
+    "fastfood": "cafe",
+    "streetfood": "cafe",
+    "фастфуд": "cafe",
+
+    "интернет": "communication",
+    "internet": "communication",
+    "tv": "communication",
+    "тв": "communication",
+    "телефоны": "communication",
+    "телефон": "communication",
+
+    "отпуск": "vacation",
+}
