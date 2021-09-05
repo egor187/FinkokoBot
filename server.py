299d7cb33ad34bdc57011c9b6e96a1d0055e0297
egor187 <py.egor.py@gmail.com>
2021-09-05 18:55:11 +0300

    testing webhook settings 2

--- server.py
+++ server.py
@@ -13,25 +13,25 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-HEROKU_WEBHOOK = os.getenv("HEROKU_WEBHOOK")
+# HEROKU_WEBHOOK = os.getenv("HEROKU_WEBHOOK")
 
 # webhook settings
 WEBHOOK_HOST = "https://finko-bot.herokuapp.com/"
 WEBHOOK_PATH = f'/webhook/{TELEGRAM_BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-WEBAPP_HOST = 'localhost'  # or ip
-WEBAPP_PORT = 3001
+WEBAPP_HOST = '0.0.0.0'  # or ip
+WEBAPP_PORT = int(os.getenv("PORT"))
 
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dispatcher = Dispatcher(bot)
 
 
-async def on_startup(dp):
-    await bot.set_webhook(url=HEROKU_WEBHOOK)
+async def on_startup(dispatcher: Dispatcher) -> None:
+    await bot.set_webhook(url=WEBHOOK_URL)
 
 
-async def on_shutdown(dp):
+async def on_shutdown(dispatcher):
     await bot.delete_webhook()
 
 
@@ -83,7 +83,7 @@ if __name__ == "__main__":
         dispatcher=dispatcher,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
-        on_shutdown=on_shutdown,
+        # on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
