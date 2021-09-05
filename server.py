2a2a31df252222450a9f7495c302ab39684f9154
egor187 <py.egor.py@gmail.com>
2021-09-05 18:17:41 +0300

    testing webhook settings

--- server.py
+++ server.py
@@ -15,6 +15,13 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 HEROKU_WEBHOOK = os.getenv("HEROKU_WEBHOOK")
 
+# webhook settings
+WEBHOOK_HOST = "https://finko-bot.herokuapp.com/"
+WEBHOOK_PATH = f'/webhook/{TELEGRAM_BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_PORT = 3001
+
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dispatcher = Dispatcher(bot)
@@ -74,7 +81,10 @@ if __name__ == "__main__":
     # executor.start_polling(dispatcher, skip_updates=False)
     executor.start_webhook(
         dispatcher=dispatcher,
-        webhook_path=HEROKU_WEBHOOK,
+        webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
-        on_shutdown=on_shutdown
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
     )
