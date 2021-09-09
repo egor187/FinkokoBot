face0c9f057ef4541953eeb1b42d8e3c51092bd4
egor187 <py.egor.py@gmail.com>
2021-09-09 12:05:38 +0300

    remove "on_shutdown" handler

--- server.py
+++ server.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
         dispatcher=dispatcher,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
-        on_shutdown=on_shutdown,
+        # on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
