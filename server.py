f40e627c49978964369d4f73c3834c1ec26851ef
egor187 <py.egor.py@gmail.com>
2021-09-11 23:06:39 +0300

    return "on_startup" arg on executor

--- server.py
+++ server.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     executor.start_webhook(
         dispatcher=dispatcher,
         webhook_path=WEBHOOK_PATH,
-        # on_startup=on_startup,
+        on_startup=on_startup,
         # on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
