7fbd8b8184cbf16c01eee32a621fba27a764611a
egor187 <py.egor.py@gmail.com>
2021-09-19 17:17:16 +0300

    addded state machine for handle conversation with bot about setting budget

--- bot/server.py
+++ bot/server.py
@@ -1,9 +1,12 @@
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
 from loguru import logger
 import os
 
 import dotenv
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 # import db
 import exceptions, pg_db as db
@@ -23,7 +26,12 @@ WEBAPP_PORT = int(os.getenv("PORT", 5000))
 
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
-dispatcher = Dispatcher(bot)
+storage = MemoryStorage()
+dispatcher = Dispatcher(bot, storage=storage)
+
+
+class BudgetState(StatesGroup):
+    month_limit = State()
 
 
 async def on_startup(dp: Dispatcher) -> None:
@@ -68,9 +76,30 @@ async def delete_last_payment(message: types.Message):
         await message.answer("Last payment deleted")
 
 
-@dispatcher.message_handler()
-async def add_payment_view(message: types.Message):
-    """Entrypoint to 'add_payment' process"""
+@dispatcher.message_handler(commands=["set_budget"])
+async def set_month_budget(message: types.Message):
+    await BudgetState.month_limit.set()
+    await message.answer("What is the limit of budget")
+
+
+@dispatcher.message_handler(state=BudgetState.month_limit)
+async def process_budget_limit(message: types.Message, state: FSMContext):
+
+    async with state.proxy() as data:
+        data['month_limit'] = message.text
+
+    await state.finish()
+
+    try:
+        db.set_budget(data.get("month_limit"))
+    except ValueError:
+        await message.answer("Incorrect message format. Budget amount must be an integer")
+    else:
+        await message.answer("Budget set")
+
+
+@dispatcher.message_handler(commands=["show_limit"])
+async def get_budget_balance(message: types.Message):
     try:
         db.add_payment(message)
     except (exceptions.IncorrectAmountFormatMessage, exceptions.IncorrectMessageException):
