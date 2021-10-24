8a85c818aafdd822be65ec52fb5fbde33044e4a9
egor187 <py.egor.py@gmail.com>
2021-10-24 12:24:10 +0300

    refactor retrieving category by alias process to operate in db instead outer dict of aliases

--- bot/pg_db.py
+++ bot/pg_db.py
@@ -47,10 +47,17 @@ def get_now_datetime_formatted() -> str:
 
 def _get_category_name_by_alias(category_alias: str) -> str:
     """Return category name for passed category alias"""
-    if category_alias not in aliases.aliases.values():
-        category_name = aliases.aliases.get(category_alias)
-    else:
-        category_name = category_alias
+    with connection.cursor() as cur:
+        cur.execute(f"SELECT category FROM Aliases WHERE alias = '{category_alias}'")
+        try:
+            category_id = cur.fetchone()[0]
+        except TypeError:
+            category_id = None
+        logger.error(f"!!!{category_id}")
+        if category_id:
+            category_name = _get_category_name_by_id(category_id)
+        else:
+            category_name = OTHER_CATEGORY
     return category_name
 
 
@@ -64,9 +71,28 @@ def parse_payment_message(income_message: Message) -> Optional[tuple[int, str]]:
             return amount, category
         except ValueError as e:
             logger.info(e)
-            raise exceptions.IncorrectAmountFormatMessage
+            raise exceptions.IncorrectAmountFormatMessageException
     else:
-        logger.error("Incorrect message. Need in fmt '{amount} {category}'")
+        logger.error("Incorrect message. Need in fmt '{amount} {category/alias}'")
+        raise exceptions.IncorrectMessageException
+
+
+def parse_add_category_message(income_message: str):
+    """Parse message for add new category"""
+    parsed_message = re.match(r"([\D ]+): (.*)", income_message)
+    if parsed_message:
+        try:
+            category = str(parsed_message.group(1))
+            aliases = parsed_message.group(2).split(" ")
+            logger.info(f"aliases: {aliases}")
+            if not aliases:
+                aliases = category
+            return {"category": category, "aliases": aliases}
+        except ValueError as e:
+            logger.info(e)
+            raise exceptions.IncorrectAddCategoryFormatMessageException
+    else:
+        logger.error("Incorrect message. Need in fmt '{category} {alias, ...}'")
         raise exceptions.IncorrectMessageException
 
 
@@ -88,6 +114,14 @@ def get_all_categories() -> str:
             answer += category[1] + "\n"
         return answer
     return no_query_answer
+
+
+def _get_category_name_by_id(category_id: str) -> str:
+    """Return category name by id"""
+    with connection.cursor() as cur:
+        cur.execute(f"SELECT name FROM Category WHERE id = '{category_id}'")
+        category_name = cur.fetchone()[0]
+    return category_name
 
 
 def _get_category_id_by_name(category_name: str) -> int:
@@ -151,12 +185,6 @@ def add_payment(income_message: Message) -> None:
         amount, category_name = parse_payment_message(income_message)
     except (exceptions.IncorrectAmountFormatMessageException, exceptions.IncorrectMessageException):
         raise
-
-    all_categories = get_all_categories()
-
-    if category_name not in all_categories:
-        category_name = OTHER_CATEGORY
-
     category_id = _get_category_id_by_name(category_name)
 
     budget = _get_last_active_budget()
@@ -174,6 +202,31 @@ def add_payment(income_message: Message) -> None:
 
         if budget.get("balance") < amount:
             raise exceptions.BudgetLimitReachedException
+
+
+def add_category(income_message: str) -> None:
+    """Add new category and its aliases to db"""
+    try:
+        category_name = parse_add_category_message(income_message).get("category")
+        aliases_list = parse_add_category_message(income_message).get("aliases")
+    except (exceptions.IncorrectAddCategoryFormatMessageException, exceptions.IncorrectMessageException):
+        raise
+
+    all_categories = get_all_categories()
+    logger.info(category_name)
+    if category_name:
+        if category_name in all_categories:
+            raise exceptions.CategoryAlreadyExistsException
+        with connection.cursor() as cur:
+            cur.execute(
+                f"INSERT INTO Category(name, is_base) VALUES ('{category_name}', '{True}')"
+            )
+            category_id = _get_category_id_by_name(category_name)
+            for alias in aliases_list:
+                cur.execute(
+                    f"INSERT INTO Aliases(category, alias) VALUES ('{category_id}', '{alias}')"
+                )
+        logger.info(f"Category '{category_name}' added")
 
 
 def delete_last_payment() -> None:
